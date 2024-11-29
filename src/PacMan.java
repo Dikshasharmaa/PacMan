d5bca09ba5d8282fda64db7b697532a395284491
Diksha Sharma <diksha66sharma78@gmail.com>
2024-11-29 16:17:40 -0500

    Loaded Images

--- src/PacMan.java
+++ src/PacMan.java
@@ -22,12 +22,26 @@ public class PacMan extends JPanel{
     private Image pacmanLeftImage;
     private Image pacmanRightImage;
 
-
-
     PacMan(){
         setPreferredSize(new Dimension(boardWidth, boardHeight));
         setBackground(Color.BLACK);
 
+        //load Images
+        wallImage = new ImageIcon(getClass().getResource("./wall.png")).getImage(); //getClass refers to PacMan class
+        blueGhostImage = new ImageIcon(getClass().getResource("./blueGhost.png")).getImage();
+        orangeGhostImage = new ImageIcon(getClass().getResource("./orangeGhost.png")).getImage();
+        pinkGhostImage = new ImageIcon(getClass().getResource("./pinkGhost.png")).getImage();
+        redGhostImage = new ImageIcon(getClass().getResource("./redGhost.png")).getImage();
+
+        pacmanUpImage = new ImageIcon(getClass().getResource("./pacmanUp.png")).getImage();
+        pacmanDownImage = new ImageIcon(getClass().getResource("./pacmanDown.png")).getImage();
+        pacmanLeftImage = new ImageIcon(getClass().getResource("./pacmanLeft.png")).getImage();
+        pacmanRightImage = new ImageIcon(getClass().getResource("./pacmanRight.png")).getImage();
+
+
+
+
+        
     }
 
 }
