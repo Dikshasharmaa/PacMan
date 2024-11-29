e11451c36f9ebd36a11666080fe3f107c76c3b76
Diksha Sharma <diksha66sharma78@gmail.com>
2024-11-29 15:02:46 -0500

    Created a game frame with specified height and width

--- src/App.java
+++ src/App.java
@@ -1,6 +1,19 @@
 import javax.swing.JFrame;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int rows = 21;
+        int columns = 19;
+        int tileSize = 32;
+        int boardWidth = columns *tileSize;
+        int boardHeight = rows* tileSize;
+
+        JFrame frame = new JFrame("Pac Man"); // Creating a new frame with name Pac Man
+        frame.setVisible(true); //To make the frame visible
+        frame.setSize(boardWidth, boardHeight); // seting the size of the board
+        frame.setLocationRelativeTo(null); // to keep the frame in center
+        frame.setResizable(false); // so that the user can't resize the window
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // so that the user can exit after clicking close
+
+
     }
 }
