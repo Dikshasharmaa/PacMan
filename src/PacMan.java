abda8eb37893c1e781d776b42344378326057536
Diksha Sharma <diksha66sharma78@gmail.com>
2024-11-29 15:37:49 -0500

    Added size and bg color

--- src/PacMan.java
+++ src/PacMan.java
@@ -1,13 +1,19 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import java.uytil.HashSet;
+import java.util.HashSet;
 
 public class PacMan extends JPanel{
-    int rows = 21;
-    int columns = 19;
-    int tileSize = 32;
-    int boardWidth = columns *tileSize;
-    int boardHeight = rows* tileSize;
+    private int rows = 21;
+    private int columns = 19;
+    private int tileSize = 32;
+    private int boardWidth = columns *tileSize;
+    private int boardHeight = rows* tileSize;
+
+    PacMan(){
+        setPreferredSize(new Dimension(boardWidth, boardHeight));
+        setBackground(Color.BLACK);
+        
+    }
 
 }
