514b6e536c0576b97de4aef195c7416ba2b39d72
Diksha Sharma <diksha66sharma78@gmail.com>
2024-11-29 15:23:21 -0500

    Created a new classwhich extends JPanel

---
+++ src/PacMan.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.uytil.HashSet;
+
+public class PacMan extends JPanel{
+    int rows = 21;
+    int columns = 19;
+    int tileSize = 32;
+    int boardWidth = columns *tileSize;
+    int boardHeight = rows* tileSize;
+
+}
