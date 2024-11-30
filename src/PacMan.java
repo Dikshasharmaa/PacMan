63dd2764939390134852e2fec6b165e52258ab78
Diksha Sharma <diksha66sharma78@gmail.com>
2024-11-30 15:03:41 -0500

    Draw all the objects on the tile map

--- src/PacMan.java
+++ src/PacMan.java
@@ -152,6 +152,14 @@ public class PacMan extends JPanel{
         for(Block ghost: ghosts){
             g.drawImage(ghost.image, ghost.x, ghost.y, ghost.width, ghost.height, null);
         }
+        for(Block wall: walls){
+            g.drawImage(wall.image, wall.x, wall.y, wall.width, wall.height, null);
+        }
+        g.setColor(Color.white);
+        for(Block food: food){
+            g.fillRect(food.x, food.y, food.width, food.height);
+        }
+        
 
     }
 
