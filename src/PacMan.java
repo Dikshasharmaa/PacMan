8dbc543d32c8685cc9371652a8f144c4b22dc124
Diksha Sharma <diksha66sharma78@gmail.com>
2024-12-01 13:09:57 -0500

    Detect wall in move function

--- src/PacMan.java
+++ src/PacMan.java
@@ -204,6 +204,15 @@ public class PacMan extends JPanel implements ActionListener, KeyListener{
     public void move(){
         pacman.x += pacman.velocityX;
         pacman.y += pacman.velocityY;
+
+        //check wall collision
+        for(Block wall : walls){
+            if(collision(pacman, wall)){
+                pacman.x -= pacman.velocityX;
+                pacman.y -= pacman.velocityX;
+                break;
+            }
+        }
     }
 
     public boolean collision(Block a, Block b){
